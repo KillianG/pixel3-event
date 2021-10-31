b245a61566109cc6b412405edeebfb3483c4dc6f
Erwan Bernard <erwan.bernard079@gmail.com>
2021-10-31 11:48:39 +0100

    add pixel update and versioning

--- generate_image.py
+++ generate_image.py
@@ -1,22 +1,37 @@
 from PIL import Image
 import os
+PATH = os.environ.get('PATH_IMAGES', './tests')
 
 
-class GeneratedImage(object):
-    def __init__(self, path):
-        self.path = path
-        if not os.path.isfile(self.path):
-            img = Image.new('RGB', [1000, 1000], 255)
-            img.save(self.path)
+def string_to_tuple(color):
+    n = 2
+    color = [int(color[i:i+n], 16) for i in range(0, len(color), n)]
+    return color[0], color[1], color[2]
 
-    def edit_pixel(self, nft_id, color):
-        n = 2
-        y = nft_id // 1000
-        x = nft_id % 1000
-        color = [int(color[i:i+n], 16) for i in range(0, len(color), n)]
-        print(x, y)
-        print(color)
-        with Image.open(self.path) as im:
-            self.image = im.load()
-            self.image[x, y] = (color[0], color[1], color[2])
-            im.save(self.path)
+
+def get_pos_from_id(id):
+    y = id // 1000
+    x = id % 1000
+    return x, y
+
+
+def generate_image(color, nft_id, export_to=None):
+    path = PATH + '/default.png'
+    if not os.path.isfile(path):
+        img = Image.new('RGB', [1000, 1000], 0)
+        img.save(path)
+
+    color = string_to_tuple(color)
+    x, y = get_pos_from_id(nft_id)
+    print(x, y)
+    print(color)
+    with Image.open(path) as im:
+        image = im.load()
+        image[x, y] = color
+        im.save(path)
+        if export_to:
+            im.save(PATH + '/' + export_to)
+
+
+if __name__ == "__main__":
+    generate_image('ff00ff', 0)
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from generate_image import generate_image
+from datetime import datetime
+
+
+def save_pixel_in_versioned_image(color, nft_id):
+    versioned_path = datetime.now().isoformat() + '.png'
+    print(versioned_path)
+    generate_image(color, nft_id, versioned_path)
+
+
 if __name__ == "__main__":
-    print("Hello, NFTTTTTTTTTTT")
+    save_pixel_in_versioned_image('00ffff', 1)
